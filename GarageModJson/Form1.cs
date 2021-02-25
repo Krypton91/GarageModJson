3135a6ebef4604317c549e0c423921384af4eebf
Krypton91 <krypton1@gmx.at>
2021-02-25 01:56:01 +0100

    Initial commit

---
+++ GarageModJson/Form1.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GarageModJson
+{
+    public partial class Form1 : Form
+    {
+        public List<string> GarageModFiles = new List<string>();
+        public string ServerProfilesFolderDir { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(ServerProfilesFolderDir))
+            {
+                for (int i = 0; i < GarageModFiles.Count; i++)
+                {
+                    var DCBankingJson = File.ReadAllText(GarageModFiles[i]);
+                    var JsonMeta = new DepositaryData();
+                    JsonConvert.PopulateObject(DCBankingJson, JsonMeta);
+
+                    var GarageNewJson = new DepositaryData();
+
+                    //AdvJsonObject.m_BonusCurrency = JsonMeta.m_MaxOwnedCurrencyBonus;
+                    GarageNewJson.m_SteamID = JsonMeta.m_SteamID;
+                    GarageNewJson.m_PlayerName = JsonMeta.m_PlayerName;
+
+
+
+                    GarageNewJson.vehicleData = JsonMeta.vehicleData;
+                    if (!Directory.Exists(ServerProfilesFolderDir + @"\Depositary_System\ParsedData\"))
+                        Directory.CreateDirectory(ServerProfilesFolderDir + @"\Depositary_System\ParsedData\");
+                    using (StreamWriter file = File.CreateText(ServerProfilesFolderDir + @"\Depositary_System\ParsedData\" + JsonMeta.m_SteamID + ".json"))
+                    {
+                        JsonSerializer serializer = new JsonSerializer();
+                        serializer.Serialize(file, GarageNewJson);
+                        serializer.Formatting = Formatting.Indented;
+                    }
+                }
+
+                MessageBox.Show("DONE!");
+
+
+            }
+            else
+            {
+                MessageBox.Show("ERROR!", "ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int fileCount = 0;
+            using (var fdg = new FolderBrowserDialog())
+            {
+                DialogResult result = fdg.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fdg.SelectedPath))
+                {
+                    string[] filenames = Directory.GetFiles(fdg.SelectedPath + @"\Depositary_System\PlayerData\");
+                    ServerProfilesFolderDir = fdg.SelectedPath;
+                    foreach (string filename in filenames)
+                    {
+                        if (filename.Contains(".json"))
+                        {
+                            fileCount++;
+                            GarageModFiles.Add(filename);
+                        }
+                    }
+                    MessageBox.Show("found " + fileCount + " files \nIn folder please convert it now!");
+                    button2.Visible = true;
+                }
+            }
+        }
+    }
+}
---
+++ GarageModJson/GarageModObjects.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageModJson
+{
+
+    public class DepositaryData
+    {
+        public string m_SteamID { get; set; }
+        public string m_PlayerName { get; set; }
+
+        public List<VehicleData> vehicleData { get; set; }
+    }
+
+    public class NewDepositaryData
+    {
+        public string m_SteamID { get; set; }
+        public string m_PlayerName { get; set; }
+
+        public List<VehicleDataNew> vehicleData { get; set; }
+    }
+
+    public class VehicleData
+    {
+        public int indexID { get; set; }
+        public int VehiclesHash { get; set; }
+        public int GarageID { get; set; }
+
+        public double EngineHealth { get; set; }
+
+        public double FuelAmmount { get; set; }
+
+        public string VehiclesName { get; set; }
+
+        public Vector SpawnPos;
+        public Vector SpawnOri;
+
+        public List<VehicleCargo> m_Cargo;
+    }
+
+    public class Vector
+    {
+        public double X = 0;
+        public double Z = 0;
+        public double Y = 0;
+    }
+
+    public class VehicleCargo
+    {
+        public string ItemName;
+        public int VehicleCargoAmmount;
+        public int KeyHash;
+        public double Health;
+    }
+
+    public class VehicleDataNew
+    {
+        public int indexID { get; set; }
+        public int VehiclesHash { get; set; }
+        public int GarageID { get; set; }
+
+        public double EngineHealth { get; set; }
+
+        public double FuelAmmount { get; set; }
+
+        public string VehiclesName { get; set; }
+
+        public Vector SpawnPos;
+
+        public Vector SpawnOri;
+
+        public List<VehicleCargo> m_Cargo;
+    }
+}
